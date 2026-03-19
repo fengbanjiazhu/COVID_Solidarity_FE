e0cc2321e3f85e5756f9cce6f9a572460047997c
Jeffrey <562475805lanu@gmail.com>
2026-03-20 10:59:26 +1100

    Added input and card, label components, added example of shadcn ui codes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { VenueSearch } from "../components/Venues/VenueSearch";
 function Home() {
   return (
     <div className="w-full p-2">
       <h1 className="text-4xl mb-8">Home</h1>
+      <VenueSearch />
     </div>
   );
 }
